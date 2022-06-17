6706ae00e13930173e5d74f03d0f0ab57b73f88d
Sadhna kumari <sadhhnakhairia1998@gmail.com>
2022-06-17 20:07:33 +0530

    classwork17june

---
+++ abc.js
@@ -0,0 +1,26 @@
+// <--------------------------1st question--------------------------->
+let n = parseInt(process.argv[2]);
+for(let i = 1; i<=n; i++){
+    console.log(i);
+}
+
+// <--------------------------2nd question--------------------------->
+
+
+let a = parseInt(process.argv[2])
+let b = parseInt(process.argv[3])
+for(let i=a+1; i<=b; i++){
+  if(i%2==0){
+    console.log(i);
+  }
+}
+// <------------------------------3rd question------------------------>
+
+
+let n = parseInt(process.argv[2]);
+for(let i = n; i>=1; i--){
+    console.log(i);
+}
+
+
+
